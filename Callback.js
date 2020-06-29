51294565119a369c93ae124f41faa70504814126
Harris-Butt <33513710+Harris-Butt@users.noreply.github.com>
2020-06-29 22:21:36 +0500

    Update Callback.js

--- Callback.js
+++ Callback.js
@@ -1,7 +1,34 @@
 console.log("Start")
 
-setTimeout(()=>{
-console.log("Middle")
-},3000);
+function loginUser(email,password,callBack){
+    setTimeout(()=>{
+        console.log("Now we have the data");
+        callBack( {"userEmail": email})
+    },2000);
+}
+function Video(email,callBack){
+    setTimeout(()=>{
+        callBack(['video1','video2']);
+    },2000)
+   
+}
+function videoDetails(video,callBack){ 
+    setTimeout(()=>{
+        callBack(`title of the video is `);
+    })
+}
+const user = loginUser("harr@gmail.com",
+                        123456,
+                        (user)=>
+                        {console.log(`user is ${user.userEmail}`);
+                        Video(user.userEmail,
+                        (vid)=>
+                        {
+                            console.log(vid);
+                            videoDetails(vid[0],(title=>{
+                                console.log(title);
+                            }))
+                        }
+                        )}); 
 
-console.log("End")
+console.log("Finish");
